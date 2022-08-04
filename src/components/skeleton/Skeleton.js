33e451193315e744e73c01b6c8fd214800422389
Andrii <ndrwkos@gmail.com>
2022-08-05 00:14:26 +0300

    Fixed skeleton

--- src/components/skeleton/Skeleton.js
+++ src/components/skeleton/Skeleton.js
@@ -3,7 +3,6 @@ import './Skeleton.scss';
 const Skeleton = () => {
     return (
         <div className="skeleton">
-            <p className="char__select">Please select a character to see information</p>
             <div className="pulse skeleton__header">
                 <div className="pulse skeleton__circle"></div>
                 <div className="pulse skeleton__mini"></div>
